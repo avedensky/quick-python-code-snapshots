d6eb2f74187beaaf403ee0c48256bb463fff1a04
Alexey Vedensky <avedensky@gmail.com>
2017-06-22 19:12:13 +0300

    Refact: urlib sample

--- urllib/urlib_post.py
+++ urllib/urlib_post.py
@@ -6,32 +6,11 @@
 import urllib
 import urllib2
 
-
-
-
-
-#proxys = "192.168.23.4:8080" # в формате прокси:порт
-#user = "login" # прокси логин
-#password = "password" # прокси пароль
-
-
-
-#proxy = urllib2.ProxyHandler({"http" : proxys}) #инициализация 
-# - пока не нужно -    proxy_auth_handler = urllib2.ProxyBasicAuthHandler() # инициализация авторизации в прокси
-# - пока не нужно -    proxy_auth_handler.add_password('realm', 'uri', user, password) # добавляем логин и пароль в прокси
-
-# собираем загрузчик прокси, авторизации и кукилиба
-#opener = urllib2.build_opener(proxy,здесь еще handler ntlm авторизации должен быть)
-#urllib2.install_opener(opener) # устанавливаем загрузчик
-
-
-
 headers = {'User­Agent' : 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
 values = {'Host' : 'whois.ripn.net', 'Whois' : 'ya.ru'}
 data = urllib.urlencode(values) #преобразовывает текст в строку key=value&key=value
 
 
 req = urllib2.Request('http://www.ripn.net/nic/whois/whois.cgi', data, headers)
-#response = opener.open(req)
 response = urllib2.urlopen(req)
 print response.read()
