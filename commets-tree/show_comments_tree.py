83be812067b8fcae5c137de5690e220861cccec8
Alexey Vedensky <avedensky@gmail.com>
2017-09-11 19:57:06 +0300

    Chg comment-tree

--- commets-tree/show_comments_tree.py
+++ commets-tree/show_comments_tree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #coding: utf-8
 
 """
@@ -22,14 +22,14 @@ I am Third
 from operator import itemgetter
 
 mlst = [ #Table 'comments' in Data Base
-    {'id':1, 'parrent_id':0, 'path':'', 'level':0, 'message':'What is mean : green'}, 
-    {'id':2, 'parrent_id':0, 'path':'', 'level':0, 'message':'Vasya here is'},
+    {'id':1, 'parrent_id':0, 'path':'', 'level':0, 'message':'What is mean word - green'}, 
+    {'id':2, 'parrent_id':0, 'path':'', 'level':0, 'message':'Vasya is here.'},
     {'id':3, 'parrent_id':0, 'path':'', 'level':0, 'message':'I am Third'},
     {'id':4, 'parrent_id':1, 'path':'', 'level':0, 'message':'Green is color'},
     {'id':5, 'parrent_id':1, 'path':'', 'level':0, 'message':'Green is color of apple'},
     {'id':6, 'parrent_id':4, 'path':'', 'level':0, 'message':'I don\'t think' },
     {'id':7, 'parrent_id':6, 'path':'', 'level':0, 'message':'Ist true'},
-    {'id':8, 'parrent_id':2, 'path':'', 'level':0, 'message':'Vasya to kick'},
+    {'id':8, 'parrent_id':2, 'path':'', 'level':0, 'message':'Vasya to kick by admin'},
     {'id':9, 'parrent_id':8, 'path':'', 'level':0, 'message':'Ura!'},
     {'id':10, 'parrent_id':9, 'path':'', 'level':0, 'message':'Aha!'},
 ]
@@ -37,7 +37,7 @@ mlst = [ #Table 'comments' in Data Base
 def show(lst):
     ''' Show to display '''
     for i in lst:              
-        print(' '*(i['level']*5)+i['message'])
+        print(' ' * (i['level'] * 5) + i['message'])
         
 
 def get_parrent_id(lst, id):    
@@ -45,23 +45,34 @@ def get_parrent_id(lst, id):
         if i['id'] == id:
             return i['parrent_id']
 
-def main():
-    for i in mlst:        
-        if i['parrent_id'] == 0:
+
+def build_paths(lst):
+    ''' 
+    For every comment, calculate path to first of discuss comment
+    path is a string type and format as: <id>.<id>.<id> ...
+    3.12.  or 1.10.5. or 5.13.99.11.33. ...
+    For easy sorting after
+    '''
+    for i in lst:        
+        if i['parrent_id'] == 0: # 0 is a root comment
             i['path'] = str (i['id'])
             continue
 
-        t_id = i['parrent_id']
-        s = ''
-        level = 0
-        while t_id!=0:
-            s += '.'+str(t_id)
-            level +=1
-            t_id = get_parrent_id(mlst, t_id)            
+        tmp_id = i['parrent_id']
+        tmp_path = ''
+        tmp_level = 0
+        while tmp_id != 0:
+            tmp_path += '.' + str(tmp_id)
+            tmp_level += 1
+            tmp_id = get_parrent_id(lst, tmp_id)            
 
-        i['level'] = level
-        i['path'] = s[::-1]
+        i['level'] = tmp_level
+        i['path'] = tmp_path[::-1] #reverse string: .5.4.1 to 1.4.5.
 
+
+
+def main():
+    build_paths(mlst)
     mlst.sort(key=itemgetter('path'))
     show(mlst)
 
